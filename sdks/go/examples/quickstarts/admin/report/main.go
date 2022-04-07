5141af3cc55d918a5a19b5de2cb69f9a30f3a886
Yu Watanabe <y-watanabe@creationline.com>
2022-04-07 17:54:08 +0900

    chore: add log

--- sdks/go/examples/quickstarts/admin/report/main.go
+++ sdks/go/examples/quickstarts/admin/report/main.go
@@ -45,7 +45,7 @@ func main() {
 			break
 		}
 
-		log.Printf("Page: %v , Received %v items", pageCnt, len(resp.Items))
+		log.Printf("Page: %v, Received %v items", pageCnt, len(resp.Items))
 		for _, v := range resp.Items {
 			log.Printf("Activity Record: Id.Time: %v , Id.ApplicationName: %v, Id.UniqueQualifier: %v, Num of Events: %v",
 				v.Id.Time,
@@ -54,12 +54,13 @@ func main() {
 				len(v.Events))
 		}
 
-		if resp.NextPageToken == "" {
+		if &(resp.NextPageToken) == nil {
 			break
 		}
 
 		pageCnt++
 		*nextPageToken = resp.NextPageToken
+		log.Printf("NextPageToken: %v", *nextPageToken)
 
 		time.Sleep(5)
 	}
