5ed1fcf337a54d992584fcf14e84ead8062fd59c
Yu Watanabe <y-watanabe@creationline.com>
2022-04-08 01:01:02 +0900

    wip

--- sdks/python/examples/quickstarts/admin_report.py
+++ sdks/python/examples/quickstarts/admin_report.py
@@ -3,8 +3,11 @@
 import sys
 from pathlib import Path
 
-PACKAGE_ROOT = str(Path(__file__).parent.parent.parent.resolve())
+PACKAGE_ROOT = str(Path(__file__).resolve().parent.parent.parent)
 sys.path.append(PACKAGE_ROOT)
+
+from google_api_tutorials.factories.credential import CredentialFactory
+from google_api_tutorials.factories.service import ServiceFactory
 
 if __name__ == "__main__":
 
