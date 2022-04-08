02dfefa26960a9ae01d65f25ff621187d41f9d53
Yu Watanabe <y-watanabe@creationline.com>
2022-04-08 11:52:53 +0900

    feat: Add main for report api

--- sdks/python/examples/quickstarts/admin_report.py
+++ sdks/python/examples/quickstarts/admin_report.py
@@ -1,6 +1,9 @@
 """ """
-
+import datetime
 import sys
+import logging
+import time
+import os
 from pathlib import Path
 
 PACKAGE_ROOT = str(Path(__file__).resolve().parent.parent.parent)
@@ -11,5 +14,47 @@ from google_api_tutorials.factories.service import ServiceFactory
 
 if __name__ == "__main__":
 
-    print(PACKAGE_ROOT)
-    print(sys.path)
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    scopes = ["https://www.googleapis.com/auth/admin.reports.audit.readonly"]
+    subject_email = os.getenv("GOOGLE_SUBJECT_EMAIL")
+    next_page_token = ""
+    start_time = datetime.datetime.utcnow() - datetime.timedelta(hours=96)
+    page_cnt = 1
+
+    creds = CredentialFactory.create_with_subject(scopes,subject_email)
+    service = ServiceFactory("admin", "reports_v1", creds)
+
+    while True:
+        params = {
+            "userKey" : "all",
+            "applicationName": "drive",
+            "startTime": start_time.isoformat()
+        }
+
+        if next_page_token != "":
+            params["pageToken"] = next_page_token
+
+        resp = service.activities().list(**params)
+
+        items = resp.get('items', [])
+
+        logger.info("Page: %s, Received %d items", page_cnt, len(items))
+        for item in items:
+            logger.info("Activity Record: Id.Time: %s , Id.ApplicationName: %s, "
+                        "Id.UniqueQualifier: %s, Num of Events: %d",
+                        item['id']['time'],
+                        item['id']['applicationName'],
+                        item['id']['uniqueQualifier'],
+                        len(item['events']))
+
+        if "nextPageToken" not in resp:
+            break
+
+        logger.info("NextPageToken: %s", resp["NextPageToken"])
+        next_page_token = resp.get('nextPageToken')
+        page_cnt += 1
+
+        time.sleep(5)
